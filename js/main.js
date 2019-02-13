97d5f6425d539c6d4c0a9863af2f14c354825038
Nishigandha Sontakke <sontakke@wisc.edu>
2019-02-13 00:55:08 -0600

    Updated main.js
    Updated main.js

--- js/main.js
+++ js/main.js
@@ -106,82 +106,28 @@ $(document).ready(function(){
     initialize();
 });
 
+
+
 function jsAjax(){
-    // Step 1: Create the request 
-    var ajaxRequest = new XMLHttpRequest();
+    //define a variable to hold the data
+    var mydata;
 
-    //Step 2: Create an event handler to send received data to a callback function
-    ajaxRequest.onreadystatechange = function(){
-        if (ajaxRequest.readyState == 4){
-            callback(ajaxRequest.response);
-        };
-    };
-
-    //Step 3: Open the server connection
-    ajaxRequest.open('GET', 'data/megacity.geojson', true);
-
-    //Step 4: Set the response data type
-    ajaxRequest.responseType = "json";
-
-    //Step 5: Send the request
-    ajaxRequest.send();
-};
-
-
-//define callback function
-function callback(response){
-
-    var mydata = response;
-
-    //pass data to another function
-    nextFunction(mydata);
-};
-
-window.onload = jsAjax();
-//define AJAX function
-function jQueryAjax(){
-	var mydata;
-    //basic jQuery ajax method
+    //ajax method
     $.ajax("data/megacity.geojson", {
         dataType: "json",
         success: function(response){
             mydata = response;
+
+            //check the data
+            console.log(mydata); // This the data
         }
-	});
-	console.log(mydata);
-};
-
-
-
-function nextFunction(data){
-
-    console.log(data); //contains response data held by mydata in callback
-};
-
-$(document).ready(jQueryAjax);
-//a simple counting function
-function countToThree(){
-    var count = 0;
-    while (count < 3){
-        count++;
-    };
-    return count;
-};
-
-var mydata = countToThree();
-
-console.log(mydata); //mydata is 3
-
-function jQueryAjax(){
-    var mydata = $.ajax("data/megacity.geojson", {
-        dataType: "json"
     });
-    return mydata;
+
+    //check the data
+    console.log(mydata); // Data cannot be accessed outside of the callback function
 };
 
-var mydata = jQueryAjax();
-
-console.log(mydata); //the jQuery XMLHttpRequest object
+$(document).ready(jsAjax);
 
 
 //Debug.js 
@@ -192,6 +138,7 @@ function debugCallback(response){
 	$(mydiv).append('<br><b>GeoJSON data:</b><br>'+ JSON.stringify(response));// using stringify to convert JavaScript object to string, since 
 																			//when sending data to a web server, the data has to be a string
 };
+
 //defining AJAX function
 function debugAjax(){
 	//ajax method
